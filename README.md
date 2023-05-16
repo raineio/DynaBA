7c1fc3949c261784b529aaab263ca5d40edec403
Raine <64136899+raineio@users.noreply.github.com>
2023-05-16 16:15:50 +0200

    add docs and dummy config

---
+++ README.md
@@ -0,0 +1,13 @@
+# DynaBA/PDP BA Helper
+
+## Setting up and running
+
+- Python 3.10 installed at the very least
+
+Using venv:
+
+- Run `python -m venv /path/to/new/virtual/environment`, recommended path; `.\dynaba`
+- Once the venv has been set up, run `pip install -r requirements.txt` to install pycord and yaml
+- Rename the file called dummy-config.yml, and insert your bots token in the token slot.
+
+Your bot should now run perfectly with the `python .\main.py` command!
--- main.py
+++ main.py
@@ -13,36 +13,36 @@ async def on_ready():
 
 @bot.slash_command()
 async def portal_map(ctx: discord.ApplicationContext):
-    embed = discord.Embed(title = config["portal_map"]["title"], description = "")
+    embed = discord.Embed(title = config["portal_map"]["title"], description = config["portal_map"]["description"])
     embed.add_field(name="", value=config["portal_map"]["content"])
     embed.set_image(url = config["portal_map"]["image"])
 
     await ctx.respond(embed = embed)
 
 @bot.slash_command()
-async def macros(ctx: discord.ApplicationContext):
-    embed = discord.Embed(title=config["portal_assignments"]["title"], description="")
+async def portal_macro(ctx: discord.ApplicationContext):
+    embed = discord.Embed(title=config["portal_assignments"]["title"], description = config["portal_assignments"]["description"])
     embed.add_field(name="", value=config["portal_assignments"]["content"])    
 
     await ctx.respond(embed = embed)
 
 @bot.slash_command()
 async def first_timer(ctx: discord.ApplicationCommand):
-    embed = discord.Embed(title = config["first_timer"]["title"], description="")
+    embed = discord.Embed(title = config["first_timer"]["title"], description = config["first_timer"]["description"])
     embed.add_field(name="", value = config["first_timer"]["content"])
 
     await ctx.respond(embed = embed)
 
 @bot.slash_command()
 async def magicite(ctx: discord.ApplicationCommand):
-    embed = discord.Embed(title = config["magia"]["title"], description="")
+    embed = discord.Embed(title = config["magia"]["title"], description = config["magia"]["description"])
     embed.add_field(name="", value = config["magia"]["content"])
 
     await ctx.respond(embed = embed)
 
 @bot.slash_command()
 async def faeries(ctx: discord.ApplicationCommand):
-    embed = discord.Embed(title = config["faerie_map"]["title"], description="")
+    embed = discord.Embed(title = config["faerie_map"]["title"], description = config["faerie_map"]["description"])
     embed.add_field(name="", value = config["faerie_map"]["content"])
     embed.set_image(url = config["faerie_map"]["image"])
 
