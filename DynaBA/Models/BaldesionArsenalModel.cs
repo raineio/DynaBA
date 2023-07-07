a9c339e345f92a19cb31ed6a260bc9302e1d24a2
Raine <64136899+raineio@users.noreply.github.com>
2023-07-07 12:08:05 +0200

    commit forgotten files

---
+++ DynaBA/Models/BaldesionArsenalModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DynaBA.Models;
+
+public struct BaldesionArsenal
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+    public string? Image { get; set; }
+}
