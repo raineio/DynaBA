4bb5a67950c2349f74206351b849558022b5188c
Raine <64136899+raineio@users.noreply.github.com>
2023-07-19 07:45:39 +0200

    finalize eureka bis

--- DynaBA/Program.cs
+++ DynaBA/Program.cs
@@ -87,66 +87,48 @@ internal class Program
                     {
                         case "bis":
                             var gearType = (GearType)data.Options.First().Value;
-                            var sb = new StringBuilder();
-                            var titleSb = new StringBuilder();
+                            var description = """
+                                              There are a few differing opinions on what Best in Slot is, but here is a general primer on what the Eurekan Best in Slot is.
+                                                 
+                                              **__First off, why do we want Elemental +2 gear?__**
+                                              
+                                              All jobs want Elemental +2 gear, to varying degrees, because of the elemental bonus you get from it.
+                                              
+                                              {1}
+                                              
+                                              **__How do I go about getting what I need to get my Best in Slot?__**
+                                                  
+                                              Getting Eureka Fragments to upgrade your Elemental +1 gear can only be gotten from the Baldesion Arsenal, of which you get 28 per full run.
+                                                  
+                                              Gearset for your job: [etro.gg]({0})
+                                              
+                                              Remember to look at the notes further down the page!
+                                              """;
+                            var title = "Eurekan Best in Slot for {0}";
 
-                            titleSb.Append("Eurekan Best in Slot for ");
-
-                            sb.Append(
-                                """
-                                    There are a few differing opinions on what Best in Slot is, but here is a general primer on what the Eurekan Best in Slot is.
-                                    
-                                    **__First off, why do we want Elemental +2 gear?__**
-
-                                    All jobs want Elemental +2 gear, to varying degrees, because of the elemental bonus you get from it. 
-                                    Healers/Casters want the least of it, considering their Best in Slot chest piece, the Vermilion Cloak, covers their head as well.
-                                
-                                    **__How do I go about getting what I need to get my Best in Slot?__**
-                                    
-                                    Getting Eureka Fragments to upgrade your Elemental +1 gear can only be gotten from the Baldesion Arsenal, of which you get 28 per full run.
-                                    
-                                    Gearset for your job: [etro.gg]
-                                """
-                            );
-
-
-                            switch (gearType)
+#pragma warning disable CS8524 
+                            var (t, link, special) = gearType switch
+#pragma warning restore CS8524
                             {
-                                case GearType.Fending:
-                                    titleSb.Append("Tanks");
-                                    sb.Append("(https://etro.gg/gearset/ccdfa90d-a16c-4b9e-a5b7-c6b0d3df6fea)");
-                                    break;
-                                case GearType.Healing:
-                                    titleSb.Append("Healers");
-                                    sb.Append("(https://etro.gg/gearset/666db791-128a-43de-ae8a-5b0a89c0fefd)");
-                                    break;
-                                case GearType.Striking:
-                                    titleSb.Append("Monk and Samurai");
-                                    sb.Append("(https://etro.gg/gearset/fcf3ce10-8891-4fe1-9db8-2c2c80be2fd5)");
-                                    break;
-                                case GearType.Scouting:
-                                    titleSb.Append("Ninja");
-                                    sb.Append("(https://etro.gg/gearset/cb75f756-9825-4688-a977-9a6ee796988)");
-                                    break;
-                                case GearType.Maiming:
-                                    titleSb.Append("Dragoon and Reaper");
-                                    sb.Append("(https://etro.gg/gearset/88c62a36-8489-4b30-80f1-c8b932bdf15d)");
-                                    break;
-                                case GearType.Aiming:
-                                    titleSb.Append("Ranged");
-                                    sb.Append("(https://etro.gg/gearset/c5ba63fb-a671-4716-b35e-c834c863f8b0)");
-                                    break;
-                                case GearType.Casting:
-                                    titleSb.Append("Casters");
-                                    sb.Append("(https://etro.gg/gearset/b573db1c-97fa-4c34-9e49-48a6bd59b1ae");
-                                    break;
-                            }
+                                GearType.Fending => ("Tanks",
+                                    "https://etro.gg/gearset/ccdfa90d-a16c-4b9e-a5b7-c6b0d3df6fea", ""),
+                                GearType.Healing => ("Healers",
+                                    "https://etro.gg/gearset/666db791-128a-43de-ae8a-5b0a89c0fefd",
+                                    "Healers can not equip a head piece, considering that the Vermilion Cloak, covers their head as well."),
+                                GearType.Striking => ("Monk and Samurai",
+                                    "https://etro.gg/gearset/fcf3ce10-8891-4fe1-9db8-2c2c80be2fd5", ""),
+                                GearType.Scouting => ("Ninja",
+                                    "https://etro.gg/gearset/cb75f756-9825-4688-a977-9a6ee796988", ""),
+                                GearType.Maiming => ("Dragoon and Reaper",
+                                    "https://etro.gg/gearset/88c62a36-8489-4b30-80f1-c8b932bdf15d", ""),
+                                GearType.Aiming => ("Ranged",
+                                    "https://etro.gg/gearset/c5ba63fb-a671-4716-b35e-c834c863f8b0", ""),
+                                GearType.Casting => ("Casters",
+                                    "https://etro.gg/gearset/b573db1c-97fa-4c34-9e49-48a6bd59b1ae",
+                                    "Casters can not equip a head piece, considering that the Vermilion Cloak, covers their head as well."),
+                            };
 
-                            sb.Append("\nRemember to look at the notes further down the page!");
-
-
-                            embed.WithTitle(titleSb.ToString()).WithDescription(sb.ToString());
-
+                            embed.WithTitle(string.Format(title, t)).WithDescription(string.Format(description, link, special));
                             break;
                     }
 
